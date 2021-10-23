e6af1e14a3ba1169ed8a460714d316c07a85ce0c
toptaskalender <toptaskalender@gmail.com>
2021-10-23 20:50:42 +0300

    Add comments to userdatabase methods

--- database/user-database.js
+++ database/user-database.js
@@ -3,15 +3,19 @@ const { User }      = require('../models');
 
 class UserDatabase extends BaseDatabase {
   async findByName(name) {
-    const user = await this.findBy('name', name);
+    /* const user = await this.findBy('name', name);
 
-    return user;
+    return user; */
+
+    return this.findBy('name', name);
   }
   
   async findByUsername(username) {
-    const user = await this.findBy('username', username);
+    /* const user = await this.findBy('username', username);
     
-    return user;
+    return user; */
+
+    return this.findBy('username', username);
   }
 };
 
