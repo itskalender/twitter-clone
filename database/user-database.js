4dce83e1474d69e290723a22fb858b0e2ff79252
toptaskalender <toptaskalender@gmail.com>
2021-10-23 12:11:46 +0300

    Add findByUsername service

--- database/user-database.js
+++ database/user-database.js
@@ -2,12 +2,14 @@ const BaseDatabase  = require('./base-database');
 const { User }      = require('../models');
 
 class UserDatabase extends BaseDatabase {
-  findByName(name){
+  findByName(name) {
     return this.findBy('name', name);
   }
-  /*
-    findByUsername
-  */
+  
+  findByUsername(username) {
+    return this.findBy('username', username);
+  }
+  
 };
 
 module.exports = new UserDatabase(User)
