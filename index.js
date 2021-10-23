5ad83eca56183a2de20945cdca307469d5e98f18
toptaskalender <toptaskalender@gmail.com>
2021-10-23 14:06:30 +0300

    Change index.js and update lib methods

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-const { userDatabase } = require('./database');
+const { userDatabase }  = require('./database');
+const { 
+  getUsers,
+  printUsernames,
+  printTweets 
+}                       = require('./lib');
+const { Tweet }         = require('./models');
 
 const kalender  = userDatabase.findByName('Kalender');
-const ozden     = userDatabase.findBy('username', 'kaynarcaozden') 
+
+const tweet1 = new Tweet(kalender, 'Hi, being a member of Twitter is very cool!') 
+kalender.tweet(tweet1);
+
+userDatabase.update([kalender])
+
+printTweets(kalender);
