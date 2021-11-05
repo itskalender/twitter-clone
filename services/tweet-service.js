1c132f4d6e687add6e76bdacf644a5c7a373c10d
toptaskalender <toptaskalender@gmail.com>
2021-11-05 18:33:09 +0300

    Update tweet and deleteTweet services

--- services/tweet-service.js
+++ services/tweet-service.js
@@ -1,17 +1,28 @@
 const BaseService = require('./base-service');
 const userService = require('./user-service');
-const { Tweet }   = require('../models');
+const { Tweet, User }   = require('../models');
 
 class TweetService extends BaseService {
   async tweet(userId, body) {
     const author  = await userService.findById(userId);
     const tweet   = await Tweet.create({ author, body });
-
+    
     author.tweets.push(tweet);
+    author.home.push(tweet);
+
+    author.followers.forEach(user => {
+      user.home.push(tweet);
+    })
+    
     await author.save();
     
     return tweet;
   };
+
+  async deleteTweet(userId, tweetId) {
+    await this.deleteById(tweetId);
+    // In this way, the tweet can be able to be removed from tweets collection, but can't from user's tweets array.
+  }
 };
 
 module.exports = new TweetService(Tweet)
