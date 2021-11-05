3467471e40b287d0bca3ea18286a75d0f2da7c4c
toptaskalender <toptaskalender@gmail.com>
2021-11-05 18:33:34 +0300

    Add follow service

--- services/user-service.js
+++ services/user-service.js
@@ -3,20 +3,25 @@ const { User }    = require('../models');
 
 class UserService extends BaseService {
   async findByName(name) {
-    /* const user = await this.findBy('name', name);
-    return user; */
-
     return this.findBy('name', name);
   }
   
   async findByUsername(username) {
-    /* const user = await this.findBy('username', username);
-    return user; */
-
     return this.findBy('username', username);
   }
 
-  
+  async follow(followerId, followingId) {
+    const follower  = await this.findById(followerId);
+    const following = await this.findById(followingId);
+
+    follower.followings.push(following);
+    following.followers.push(follower);
+
+    follower.home = follower.home.concat(following.tweets); // Ordering
+
+    await follower.save();
+    await following.save();
+  }
 };
 
 module.exports = new UserService(User)
