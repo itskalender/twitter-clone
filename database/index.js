2150ec529e10e77863034a855923ddc802fcf49f
toptaskalender <toptaskalender@gmail.com>
2021-11-05 08:56:23 +0300

    Update 'user-database', 'index.js' and add 'tweet-database'

--- database/index.js
+++ database/index.js
@@ -1,5 +1,7 @@
-const userDatabase = require('./user-database');
+const tweetDatabase = require('./tweet-database');
+const userDatabase  = require('./user-database');
 
 module.exports = {
-  userDatabase
+  userDatabase,
+  tweetDatabase
 }
--- database/user-database.js
+++ database/user-database.js
@@ -4,7 +4,6 @@ const { User }      = require('../models');
 class UserDatabase extends BaseDatabase {
   async findByName(name) {
     /* const user = await this.findBy('name', name);
-
     return user; */
 
     return this.findBy('name', name);
@@ -12,11 +11,12 @@ class UserDatabase extends BaseDatabase {
   
   async findByUsername(username) {
     /* const user = await this.findBy('username', username);
-    
     return user; */
 
     return this.findBy('username', username);
   }
+
+  
 };
 
 module.exports = new UserDatabase(User)
