b651bc132be7162e68425ebe1e9bd3d43a9da0e3
toptaskalender <toptaskalender@gmail.com>
2021-11-06 13:45:28 +0300

    Fix bug for adding tweet to users followers' home

--- services/tweet-service.js
+++ services/tweet-service.js
@@ -5,13 +5,14 @@ const { Tweet, User }   = require('../models');
 class TweetService extends BaseService {
   async tweet(userId, body) {
     const author  = await userService.findById(userId);
-    const tweet   = await Tweet.create({ author, body });
+    const tweet   = await Tweet.create({ author, body }); // const tweet = new Tweet({ author, body }).save();
     
     author.tweets.push(tweet);
     author.home.push(tweet);
 
     author.followers.forEach(user => {
       user.home.push(tweet);
+      user.save();
     })
     
     await author.save();
