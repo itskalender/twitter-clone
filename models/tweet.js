455e008427f95839c4c0d2805b1b39e0c1ce9805
toptaskalender <toptaskalender@gmail.com>
2021-11-06 20:50:52 +0300

    Update tweet model for originalTweet key

--- models/tweet.js
+++ models/tweet.js
@@ -2,22 +2,27 @@ const mongoose      = require('mongoose');
 const autopopulate  = require('mongoose-autopopulate');
 
 const tweetSchema = mongoose.Schema({
-  author      : { 
+  author        : { 
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
     required: true,
     autopopulate: { maxDepth : 1 }
   },
-  body        : { type: String, required: true },
-  retweets    : [],
-  likes       : [
+  body          : { type: String, required() { return !this.originalTweet } },
+  originalTweet : { 
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Tweet',
+    autopopulate: { maxDepth: 1 } 
+  },
+  retweets      : [],
+  likes         : [
     {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'User',
       autopopulate: { maxDepth: 1 }
     }
   ],
-  attachments : []
+  attachments   : []
 
 }, { timestamps: true })
 
