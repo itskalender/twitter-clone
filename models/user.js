50c91217bdcb4730a4073c7994ba13dd2a62da7e
toptaskalender <toptaskalender@gmail.com>
2021-11-06 20:49:50 +0300

    Update user model

--- models/user.js
+++ models/user.js
@@ -14,7 +14,7 @@ const userSchema = mongoose.Schema({
     {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'Tweet',
-      autopopulate: { maxDepth: 1 }
+      autopopulate: { maxDepth: 2 }
     }
   ],
   likedTweets : [
