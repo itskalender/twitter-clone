3f603ca0a2b7e34a825e9c97d3eeacac3c38184d
toptaskalender <toptaskalender@gmail.com>
2022-01-01 15:35:59 +0300

    Add semicolon

--- src/server.js
+++ src/server.js
@@ -6,5 +6,5 @@ configure();
 load();
 
 app.listen(process.env.APP_PORT, function startHandler() {
-  console.log(`Server listening on port: ${process.env.APP_PORT}     ✅`)
+  console.log(`Server listening on port: ${process.env.APP_PORT}     ✅`);
 });
