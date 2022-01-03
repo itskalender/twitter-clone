28c985597a47916d677082983c143a423c9654ac
toptaskalender <toptaskalender@gmail.com>
2022-01-03 21:58:29 +0300

    Update signUp controller

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -4,13 +4,15 @@ const { authService } = require('../services');
 const signUp = catchAsync(async (req, res, next) => {
   const { body: user } = req;
 
-  const account = await authService.create(user);
+  const newUser = await authService.create(user);
 
   res.status(200).json({
     code: 0,
     msg: 'success',
-    account
-  })
+    data: {
+      data: newUser
+    }
+  });
 });
 
 module.exports = {
