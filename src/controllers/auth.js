49255b6af180325d36334cf091e19cc2eb38f661
toptaskalender <toptaskalender@gmail.com>
2022-01-04 22:24:04 +0300

    Create logIn controller

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,5 +1,11 @@
-const { catchAsync }  = require('../utils');
-const { authService } = require('../services');
+const {
+  catchAsync,
+  AppError,
+  signToken
+}               = require('../utils');
+const {
+  authService
+}               = require('../services');
 
 const signUp = catchAsync(async (req, res, next) => {
   const { body: user } = req;
@@ -15,6 +21,34 @@ const signUp = catchAsync(async (req, res, next) => {
   });
 });
 
+const logIn = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  const user = await authService.findOne({ email }, '+password');
+
+  if (!user) {
+    return next(new AppError(400, 'Cannot find a user with given email or password.'));
+  }
+
+  const arePasswordsEqual = await user.comparePasswords(password, user.password);
+
+  if (!arePasswordsEqual) {
+    return next(new AppError(400, 'Cannot find a user with given email or password.'));
+  }
+
+  const JWT = signToken(user.id);
+
+  res.status(200).json({
+    code: 0,
+    msg: 'success',
+    token: JWT,
+    data: {
+      data: user
+    }
+  });
+});
+
 module.exports = {
-  signUp
+  signUp,
+  logIn
 }
