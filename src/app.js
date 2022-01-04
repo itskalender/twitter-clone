c1370856fc9bd78c76f41832b08d499330217128
toptaskalender <toptaskalender@gmail.com>
2022-01-04 21:56:32 +0300

    Implement morgan

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express     = require('express');
 const helmet      = require('helmet');
+const morgan      = require('morgan');
 const {
   authRouter
 }                 = require('./routes');
@@ -11,6 +12,7 @@ const {
 const app = express();
 
 app.use(helmet());
+app.use(morgan('dev'));
 app.use(express.json());
 
 app.use('/', authRouter);
